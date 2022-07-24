8752cc388d513ba733e59abf7471aafddb4cc528
Utkan <utkanocal@gmail.com>
2022-07-24 15:14:59 +0300

    try

--- src/components/hooks/FetchData.js
+++ src/components/hooks/FetchData.js
@@ -1,6 +1,6 @@
+const fetcher = async () => {
 
-const fetcher = async() => {
-    await fetch("https://lotrprojectapi-default-rtdb.firebaseio.com/characters.json").then(res => res.json()).then(data => data)
+   await fetch("https://lotrprojectapi-default-rtdb.firebaseio.com/characters.json").then(res => res.json()).then(data => data)
 }
 
 
